4416c348d16e9154317c2d78ee98dfe6a827123e
Jonathan Ferreira <j0n4t@hotmail.com>
2023-02-27 17:57:04 -0300

    fix: corrige informações de seed users

--- __tests__/expected_results/trybe_football_club.js
+++ __tests__/expected_results/trybe_football_club.js
@@ -1,9 +1,34 @@
 const results = {
-  users: [{
-    email: 'admin@admin.com', id: 1, password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW', role: 'admin', username: 'Admin',
-  }, {
-    email: 'user@user.com', id: 2, password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', role: 'user', username: 'User',
-  }],
+  users: [
+    {
+      email: "admin@admin.com",
+      id: 1,
+      password: "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+      role: "admin",
+      username: "Admin",
+    },
+    {
+      email: "user@user.com",
+      id: 2,
+      password: "$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO",
+      role: "user",
+      username: "User",
+    },
+    {
+      email: "@user.com",
+      id: 3,
+      password: "$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO",
+      role: "user",
+      username: "User",
+    },
+    {
+      email: "invalid.user@user.com",
+      id: 4,
+      password: "$2a$10$HDkFwOMKOI6PTza0F7.YRu1Bqsqb9hx7XkuV7QeYB5dRL4z9DI1Mu",
+      role: "user",
+      username: "User",
+    },
+  ],
   matches: [
     {
       id: 1,
@@ -391,22 +416,22 @@ const results = {
     },
   ],
   teams: [
-    { id: 1, teamName: 'Avaí/Kindermann' },
-    { id: 2, teamName: 'Bahia' },
-    { id: 3, teamName: 'Botafogo' },
-    { id: 4, teamName: 'Corinthians' },
-    { id: 5, teamName: 'Cruzeiro' },
-    { id: 6, teamName: 'Ferroviária' },
-    { id: 7, teamName: 'Flamengo' },
-    { id: 8, teamName: 'Grêmio' },
-    { id: 9, teamName: 'Internacional' },
-    { id: 10, teamName: 'Minas Brasília' },
-    { id: 11, teamName: 'Napoli-SC' },
-    { id: 12, teamName: 'Palmeiras' },
-    { id: 13, teamName: 'Real Brasília' },
-    { id: 14, teamName: 'Santos' },
-    { id: 15, teamName: 'São José-SP' },
-    { id: 16, teamName: 'São Paulo' },
+    { id: 1, teamName: "Avaí/Kindermann" },
+    { id: 2, teamName: "Bahia" },
+    { id: 3, teamName: "Botafogo" },
+    { id: 4, teamName: "Corinthians" },
+    { id: 5, teamName: "Cruzeiro" },
+    { id: 6, teamName: "Ferroviária" },
+    { id: 7, teamName: "Flamengo" },
+    { id: 8, teamName: "Grêmio" },
+    { id: 9, teamName: "Internacional" },
+    { id: 10, teamName: "Minas Brasília" },
+    { id: 11, teamName: "Napoli-SC" },
+    { id: 12, teamName: "Palmeiras" },
+    { id: 13, teamName: "Real Brasília" },
+    { id: 14, teamName: "Santos" },
+    { id: 15, teamName: "São José-SP" },
+    { id: 16, teamName: "São Paulo" },
   ],
 };
 
